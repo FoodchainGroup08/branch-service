6357d42cde34bf30ba23e1b0b15086535c20ea0f
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2026-05-07 00:32:42 +0100

    upadte branch service

---
+++ src/test/java/com/microservices/branch/BranchServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.microservices.branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
